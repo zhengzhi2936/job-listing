1085acbdeef6a8b737cba22ad6403c34134d5d9c
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-08 09:01:42 +0800

    step3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def is_admin
+    if !current_user.admin?
+      flash[:warning] = "You are not admin"
+      redirect_to root_path
+    end
+  end
 end
