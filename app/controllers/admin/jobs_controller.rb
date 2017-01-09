607e641a70514fb1aa061e772de5a389cb05904d
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-09 13:51:15 +0800

    加分题

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :require_is_admin
+  layout "admin"
   def index
     @jobs = Job.all
   end
@@ -17,18 +18,32 @@ class Admin::JobsController < ApplicationController
     @job = Job.new(job_params)
     if @job.save
       redirect_to admin_jobs_path
+    else
+      render :new
     end
   end
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
       redirect_to admin_jobs_path
-    end
+  else
+    render :edit
+  end
   end
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
     redirect_to admin_jobs_path
+  end
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+    redirect_to :back
+  end
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+    redirect_to :back
   end
   private
 
