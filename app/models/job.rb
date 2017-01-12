438a829487156e0dab3ea38630b06a0f23d74252
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-01-12 13:16:37 +0800

    step8

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,8 +5,8 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_upper_bound, presence: true
   validates :contact_email, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
-  validates :wage_upper_bound, numericality: { greater_than: 0}
+  validates :wage_lower_bound, numericality: { greater_than: 0, less_than: 100000}
+  validates :wage_upper_bound, numericality: { greater_than: 0, less_than: 100000}
   def publish!
     self.is_hidden = false
     self.save
