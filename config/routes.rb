0d1a61623324a4f04d37f61485f478c0ba482ac6
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-03-18 21:26:14 +0800

    add is_admin _to_registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :cities
-  devise_for :users
+    devise_for :user, controllers: {
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   resources :jobs do
     resources :resumes
   end
